d7c212afafbfc370a8625bed7ae3c62fee111de6
Prajjwalsen <prajjwalsen7@gmail.com>
2026-05-16 21:47:34 +0530

    Fixed render port issue

--- app.py
+++ app.py
@@ -78,5 +78,6 @@ def chat():
 )
     print("Response : ", response["answer"])
     return str(response["answer"])
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
